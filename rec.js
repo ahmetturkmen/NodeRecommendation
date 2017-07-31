bd8d02c74cd2801c9de5c9e19b6db803110f63b2
ahmetturkmen <f.ahmet.turkmen@icloud.com>
2017-07-31 15:39:38 +0300

    Intendation applied

--- rec.js
+++ rec.js
@@ -1,64 +1,63 @@
 const users = require('./users');
 // Manhattan method 
-function manhattan(user1, user2){
-    let distance=0;
-        for (let track in user1) 
-            if (track in user2) 
-             distance += Math.abs(user1[track]-user2[track])
+function manhattan(user1, user2) {
+    let distance = 0;
+    for (let track in user1)
+        if (track in user2)
+            distance += Math.abs(user1[track] - user2[track])
     return distance;
 }
 
 // Euclidean added
-function euclidean(user1,user2){
-    let distance=0;
-    for(let track of user1)
-        if(track in user2)
-            distance += ((user1[track]-user2[track])**2);           
-    
+function euclidean(user1, user2) {
+    let distance = 0;
+    for (let track of user1)
+        if (track in user2)
+            distance += ((user1[track] - user2[track]) ** 2);
+
     return Math.sqrt(distance)
 }
 
 // implementing Minkwoski Distance 
 
-function minkwoski(userRating1,userRating2,lambda){
-    let distance =0,commonRatingStatu = false;
-   for (let track in userRating1) {
-       if (userRating2.hasOwnProperty(track)) {
-        distance+=Math.abs(((userRating1[track]-userRating2[track])**lambda)); 
-       }
-   }
-     commonRatingStatu=true;           
-          if(commonRatingStatu)
-            return (distance**1/lambda);
-        return 0;
-}
-console.log(minkwoski(getTracks('Hailey'),getTracks('Jordyn'),3));
-// Computes nearest neighbor
- function computeNearestNeighbor(username, users){
-  let distances =[],userDistanceCombination;  
-  for (let user in users) 
-        if (user != username) {
-       distance =manhattan(users[user],users[username]);
-       userDistanceCombination = { "distance":distance,"user":user  };
-       distances.push(userDistanceCombination);
+function minkwoski(userRating1, userRating2, lambda) {
+    let distance = 0, commonRatingStatu = false;
+    for (let track in userRating1) {
+        if (userRating2.hasOwnProperty(track)) {
+            distance += Math.abs(((userRating1[track] - userRating2[track]) ** lambda));
+        }
     }
-    distances.sort(function(a,b){return a.distance-b.distance}) 
-  return  distances;
- }
+    commonRatingStatu = true;
+    if (commonRatingStatu)
+        return (distance ** 1 / lambda);
+    return 0;
+}
+// Computes nearest neighbor
+function computeNearestNeighbor(username, users) {
+    let distances = [], userDistanceCombination;
+    for (let user in users)
+        if (user != username) {
+            distance = manhattan(users[user], users[username]);
+            userDistanceCombination = { "distance": distance, "user": user };
+            distances.push(userDistanceCombination);
+        }
+    distances.sort(function (a, b) { return a.distance - b.distance })
+    return distances;
+}
 // Getting the tracks of the specified user
- function getTracks(user){ let tracks = users[user];  return tracks;}
+function getTracks(user) { let tracks = users[user]; return tracks; }
 
- function giveRecommendation(user) {
-    let neighborRating= getTracks(computeNearestNeighbor(user,users)[0].user)
+function giveRecommendation(user) {
+    let neighborRating = getTracks(computeNearestNeighbor(user, users)[0].user)
     let userRating = getTracks(user), recommendation = [];
-    for ( let key in neighborRating) 
-      if (neighborRating.hasOwnProperty(key) && !userRating.hasOwnProperty(key) )   
-        recommendation.push(neighborRating[key]+ '  '+key);
-    return recommendation;
+    for (let key in neighborRating)
+        if (neighborRating.hasOwnProperty(key) && !userRating.hasOwnProperty(key))
+            recommendation.push(neighborRating[key] + '  ' + key);
+    return recommendation.sort().reverse();
 }
 // console.log(computeNearestNeighbor('Chan',users))
-console.log(giveRecommendation('Hailey').sort().reverse())
-
+console.log(giveRecommendation('Hailey'))
+console.log(minkwoski(getTracks('Hailey'), getTracks('Jordyn'), 3));
 //console.log(manhattan(users['Hailey'],users['Veronica'])); // manhattan distance
 //console.log(euclidean(users['Hailey'],users['Veronica'])); // Euclidean distance 
 //console.log(computeNearestNeighbor('Chan',users));      // Nearest neighborhoods
